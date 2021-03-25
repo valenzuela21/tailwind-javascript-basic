94f2e48a967a056ec82038f5e626bb5b99a8f2df
David Fernando Valenzuela Pardo <5589477-vlzdavid12@users.noreply.gitlab.com>
2021-03-25 11:41:25 -0300

    Tailwind CSS Standard

---
+++ README.md
@@ -0,0 +1,9 @@
+### Tailwind Standard Javascript
+
+This standard base tailwind css with javascript
+
+```
+npm run build
+
+```
+[More Info](https://tailwindcss.com/)
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+    rale: ['Raleway'],
+  },
+    extend: {
+      colors: {
+        danger: '#ff5f40',
+        info: {
+          900: '#234e52',
+          800: '#285e61',
+        }
+      },
+    }
+  },
+  variants: {},
+  plugins: [],
+}
